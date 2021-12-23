e4e421542a1d624dbc6bb1aee2ccfe14294eb787
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-23 12:38:00 -0600

    added stylesheet

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const https = require("https");
+const request = require("request");
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function (request, response) {
+	response.sendFile(__dirname + "/index.html");
+})
 
 
 
