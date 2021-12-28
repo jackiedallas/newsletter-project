68ec725fa77e86cc688358111541fae172e774af
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-28 08:54:57 -0600

    fixed external css linking issues

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
+// const path = require("path");
 const app = express();
 const https = require("https");
 const request = require("request");
-app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function (request, response) {
-	response.sendFile(__dirname + "/index.html");
+	response.sendFile(__dirname + "/signup.html");
 })
 
 
